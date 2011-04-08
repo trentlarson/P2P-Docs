7cc929a3166522ecb5210848f45fee1a172122da
Trent Larson <trent@trentlarson.com>
2011-04-08 13:02:23 -0600

    for HTML files, jump to the anchor preceding the given text

--- Resources/ruby/search.rb
+++ Resources/ruby/search.rb
@@ -7,19 +7,40 @@ require 'CGI'
 
 class Search
 
-  def main(file, term)
+  # return an array of hashes: file => filename, line => line with matching text, pos => file position of beginning of line
+  def main(filename, term)
 
     lines = []
-    File.open(file) do |io|
+    pos = 0
+    File.open(filename) do |io|
       io.each do |line|
+        length = line.length # do this before chomping the line-ending characters
         line.chomp!
-        lines << { "file" => file, "line" => line } if line.include? term
+        lines << { "file" => filename, "line" => line, "pos" => pos } if line.include? term
+        pos += length
       end
     end
 
     lines
 
   end
+
+  BUFFER_LENGTH = 2048
+  # return the XML anchor name attribute value before the given position, or nil if none found in BUFFER_LENGTH beforehand
+  def previous_anchor_name(filename, filepos)
+    buffer = ""
+    File.open(filename) do |io|
+      io.seek(filepos - BUFFER_LENGTH)
+      buffer = io.read(BUFFER_LENGTH) # if it's longer than this, we'll just give up (and they'll have to search)
+    end
+    names = buffer.scan(/<a name="(.+?)"/)
+    if (names.length > 0)
+      return names.last.last
+    else
+      return nil
+    end
+  end
+
 
 end
 
