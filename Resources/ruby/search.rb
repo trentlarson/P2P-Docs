822393876a928431d070cef01da0b95d68e3b029
Trent Larson <trent@trentlarson.com>
2011-04-08 11:40:55 -0600

    remove function to create relative links for a local file

--- Resources/ruby/search.rb
+++ Resources/ruby/search.rb
@@ -1,25 +1,11 @@
 
 require 'CGI'
 
+# Usage:
 # ruby search.rb /Users/tlarson/backed/doc/family-histories/JeanGould.html dog
 # ruby -e 'load "search.rb"; Search.new.main("/Users/tlarson/backed/doc/family-histories/JeanGould.html","dog")' search.rb
 
 class Search
-
-  @locator_path = File.expand_path("locate.html")
-
-  RELATIVE_PATHS = Hash.new
-
-  def rel_to_locate(file)
-    return RELATIVE_PATHS[file] if RELATIVE_PATHS.has_key? file
-    prefix = file.dup
-    prefix.chop! while @locator_path.index(prefix) != 0
-
-    locator_after_prefix = @locator_path.sub(prefix, "")
-    up_dirs = Array.new(locator_after_prefix.count(File::SEPARATOR)).fill("..").join(File::SEPARATOR)
-    RELATIVE_PATHS[file] = file.sub(prefix, up_dirs + File::SEPARATOR)
-  end
-
 
   def main(file, term)
 
@@ -30,9 +16,6 @@ class Search
         lines << { "file" => file, "line" => line } if line.include? term
       end
     end
-
-#    links = lines.collect { |line| "<a href='locate.html?file_url=#{CGI::escape(rel_to_locate(line['file']))}'>#{line['line']}</a>" }
-#    links = lines.collect { |line| "<a href='locate.html?file_url=#{CGI::escape(line['file'])}'>#{line['line']}</a>" }
 
     lines
 
