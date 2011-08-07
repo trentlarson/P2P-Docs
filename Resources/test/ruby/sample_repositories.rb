43a5c6406997934b52d8b09dd49c949ed353e0d4
Trent Larson <trent@trentlarson.com>
2011-08-07 09:31:22 -0600

    commentary about where certain numbers of repos fail

--- Resources/test/ruby/sample_repositories.rb
+++ Resources/test/ruby/sample_repositories.rb
@@ -6,6 +6,13 @@ settings = Settings.new(base_repo_dir)
 
 # make 1000 repositories
 (0..20).each do |num|
+#21 dies on #15
+#23 dies on #15
+#25 dies on #15
+#30 dies on #14
+#50 dies on #14 (or 12)
+#100 dies on #6
+#200 dies on #27
   repo_name = "test #{num}"
   repo_dir_name = File.join(base_repo_dir, settings.fixed_repo_name(repo_name))
   settings.add_repo(repo_name, repo_dir_name)
