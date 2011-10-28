c037ca6151a4314a8d6f88616b151ff393db028b
Trent Larson <trent@trentlarson.com>
2011-10-28 16:51:17 -0600

    add better error-handling

--- Resources/js/p2pdocs.js
+++ Resources/js/p2pdocs.js
@@ -1,7 +1,35 @@
-/**
- * requires Titanium
- */
 
+function errorProps(err) {
+  var errorDebug = "";
+  if (typeof(err) != "string") {
+    for (var prop in err) {
+       errorDebug += "property: " + prop + " value: [" + err[prop] + "]\n";
+    }
+  }
+  errorDebug += "toString(): [" + err.toString() + "]\n";
+  return errorDebug;
+}
+
+/**
+ * This shows a user-friendly alert with a bunch of debugging info, including a stack trace showing function names and argument values passed in.
+ *
+ * Unfortunately, it appears that assigning window.onerror doesn't work in Titanium.
+ *
+ * requires stacktrace.js
+ */
+function p2pdocsHandleError(err, extras) {
+  message = "Sorry!  I don't understand what just happened.  If you don't mind, contact the nice people at p2pdocs.org and send them this information.  Thanks!\n\n";
+  message += errorProps(err);
+  if (extras) {
+    message += errorProps(extras);
+  }
+  message += "stack: " + printStackTrace().join('\n- ');
+  alert(message);
+}
+
+/**
+ * requires Titanium, stacktrace.js
+ */
 function viewFileWrapperFunc(sourceDir, path) {
   var viewFileFunc = function() {
     if (path == null) {
@@ -19,15 +47,8 @@ function viewFileWrapperFunc(sourceDir, path) {
           && result !== true) { // ... but I'm getting a boolean result.
         alert("Sorry!  We had a problem opening that file.  If you report this problem, the following info will help.\nfile: " + file + "\nresult: " + result);
       }
-    } catch (err) {
-      var errDebug = "";
-      if (typeof(err) != "string") {
-        for (var prop in err) {
-           errDebug += "property: " + prop + " value: [" + err[prop] + "]\n";
-        }
-      }
-      errDebug += "toString() value: [" + err.toString() + "]\n";
-      alert("Sorry!  We had a problem opening that file.  If you report this problem, the following info will help.\nfile: " + file + "\n" + errDebug);
+    } catch (e) {
+      p2pdocsHandleError(e, {"file":file});
     }
     return false;
   }
