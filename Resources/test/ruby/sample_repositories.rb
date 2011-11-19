f342e8632892617b88acd8234ab3af3c6f3c2128
Trent Larson <trent@trentlarson.com>
2011-11-19 02:44:33 -0700

    add test for many repos

--- Resources/test/ruby/sample_repositories.rb
+++ Resources/test/ruby/sample_repositories.rb
@@ -1,18 +1,38 @@
+# make many repositories
+
 require 'fileutils'
-require File.join(File.expand_path(File.dirname(__FILE__)), "../../ruby/settings.rb")
 
-base_repo_dir = File.expand_path(File.join("build", "sample-repos"))
-FileUtils.rm_rf(base_repo_dir)
-settings = Settings.new(base_repo_dir)
+class SampleManyRepos
 
-# make a bunch of repositories
-(0..50).each do |num|
-  repo_name = "test #{num}"
-  repo_dir_name = File.join(base_repo_dir, settings.fixed_repo_name(repo_name))
-  settings.add_repo(repo_name, repo_dir_name)
-  FileUtils::mkpath(repo_dir_name)
-  File.open(File.join(repo_dir_name, "test.txt"), 'w') do |out|
-    out.write("data\n")
+  # app_sources_base_dir holds our app files (under Resources)
+  # test_content_dir is where we'll put the test data
+  # app_dir is where the application properties are located
+  def self.create(app_sources_base_dir, test_content_dir)
+
+    if (app_sources_base_dir.class.name == "RubyKObject")
+      app_sources_base_dir = app_sources_base_dir.toString()
+    end
+    if (test_content_dir.class.name == "RubyKObject")
+      test_content_dir = test_content_dir.toString()
+    end
+    
+    require File.join(app_sources_base_dir, "ruby/settings.rb")
+    require File.join(app_sources_base_dir, "test/ruby/test_utils.rb")
+
+    FileUtils.rm_rf(test_content_dir)
+    settings = Settings.new(test_content_dir, Settings::BLANK_SETTINGS)
+
+    # make a bunch of repositories
+    (0..50).each do |num|
+      repo_name = "test #{num}"
+      repo_dir_name = File.join(test_content_dir, settings.fixed_repo_name(repo_name))
+      TestUtils.add_repo(settings, repo_name, repo_dir_name)
+      File.open(File.join(repo_dir_name, "test.txt"), 'w') do |out|
+        out.write("data\n")
+      end
+    end
+
   end
 end
+
 
