8205d0be5e980bc048752a39a980266be877dfc6
Trent Larson <trent@trentlarson.com>
2012-01-27 23:05:50 -0700

    add GEDCOM parser, and a test to pull out individual names

---
+++ Resources/test/ruby/gedcom-ancestry.rb
@@ -0,0 +1,80 @@
+
+#require 'gedcom'
+require File.join(File.expand_path(File.dirname(__FILE__)), "../../ruby/lib/gedcom.rb")
+
+class Individual
+  attr_accessor :id
+  attr_accessor :name
+  attr_accessor :birth_date
+
+  def initialize( id = nil, name = nil, birth_date = nil)
+    @id, @name, @birth_date = id, name, birth_date
+  end
+
+  def <=>( b )
+    name <=> b.name
+  end
+end
+
+
+class SimilarNameExtracter < GEDCOM::Parser
+  @namesToMatch = nil
+
+  def initialize
+    super
+    @currentPerson = nil
+    @allSimilar = []
+    
+    before %(INDI) do |data|
+      @currentPerson = Individual.new data[2..-2]
+    end
+
+    before %w(INDI NAME) do |data|
+      upcased = data.upcase
+      if @namesToMatch.all? { |name| upcased.match(name) }
+        @currentPerson.name = data if @currentPerson.name == nil
+        @allSimilar << @currentPerson
+      end
+    end
+
+    before %w(INDI BIRT DATE) do |data|
+      if @currentPerson.birth_date == nil
+        d = GEDCOM::Date.safe_new( data )
+        if d.is_date?
+          @currentPerson.birth_date = d
+        end
+      end
+    end
+
+    after %w(INDI) do
+      @currentPerson = nil
+    end
+  end
+
+  def setNamesToMatch(names)
+    @namesToMatch = names.upcase.split(" ")
+  end
+
+  def showSimilarPeople( )
+    puts "=- none -=" if @allSimilar.size == 0
+    @allSimilar.sort.each do |ind|
+      puts "#{ind.id}\t #{ind.name} #{ind.birth_date}"
+    end
+  end
+
+end
+
+
+parser = SimilarNameExtracter.new
+
+#if ARGV.length < 2
+#  puts "Please specify the name of a GEDCOM file and a person's name."
+#  exit(0)
+#end
+#parser.setNamesToMatch ARGV[1]
+#parser.parse ARGV[0]
+
+parser.setNamesToMatch "victoria"
+parser.parse "Resources/test/royal.ged"
+
+parser.showSimilarPeople()
