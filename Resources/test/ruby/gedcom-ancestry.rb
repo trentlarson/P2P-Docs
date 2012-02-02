6b69bb7730505c365fe777d0b17495bc86ae58ac
Trent Larson <trent@trentlarson.com>
2012-02-02 10:08:21 -0700

    OS-agnosticate the file paths

--- Resources/test/ruby/gedcom-ancestry.rb
+++ Resources/test/ruby/gedcom-ancestry.rb
@@ -1,11 +1,11 @@
 
 # doing them separately because I have to do that with Titanium; see identity.html.
-require File.join(File.expand_path(File.dirname(__FILE__)), "../../ruby/lib/gedcom_date_parser.rb")
-require File.join(File.expand_path(File.dirname(__FILE__)), "../../ruby/lib/gedcom_date.rb")
-require File.join(File.expand_path(File.dirname(__FILE__)), "../../ruby/lib/gedcom.rb")
-require File.join(File.expand_path(File.dirname(__FILE__)), "../../ruby/gedcom-ancestry.rb")
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "..", "ruby", "lib", "gedcom_date_parser.rb")
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "..", "ruby", "lib", "gedcom_date.rb")
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "..", "ruby", "lib", "gedcom.rb")
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "..", "ruby", "gedcom-ancestry.rb")
 
-file_name = ARGV.length > 0 ? ARGV[0] : "Resources/test/royal.ged"
+file_name = ARGV.length > 0 ? ARGV[0] : File.join("Resources", "test", "royal.ged")
 names_to_match = ARGV.length > 1 ? ARGV[1] : "victoria mary"
 
 parser = SimilarNameExtracter.new
@@ -17,6 +17,6 @@ parser.similarPeopleFound().each{ |elem| puts elem.to_s + "\n" }
 
 
 
-require File.join(File.expand_path(File.dirname(__FILE__)), "../../ruby/p2pdocs_utils.rb")
+require File.join(File.expand_path(File.dirname(__FILE__)), "..", "..", "ruby", "p2pdocs_utils.rb")
 
 puts parser.similarPeopleFoundJson()
